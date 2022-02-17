bc15d6346ca9cb821395fcd5645bc897bb9659db
Sophie Déziel <courrier@sophiedeziel.com>
2022-02-16 21:41:37 -0500

    Envoi des commandes MQTT

--- lib/twitchchat/client.rb
+++ lib/twitchchat/client.rb
@@ -74,9 +74,42 @@ class TwitchChat
         user    = match[:user]
         channel = match[:channel]
         content = match[:content]
-        
+
         message = Message.create(channel: channel, user: user, message: content, raw: line)
+
+        process_content(content)
+
       end
+    end
+
+    def process_content(content)
+      case
+      when content.start_with?('!cheers') && raw.match(/subscriber=1;/)
+  
+      when content.start_with?('!led ')
+        match = content.match /!led (?<couleur>.+)/
+        puts match[:couleur]
+        led(match[:couleur])
+      end
+    end
+  
+    def led(color)
+      states = {
+        "bleu" => {state: "ON", color: { r: 0, g: 0, b: 255}},
+        "rouge" => {state: "ON", color: { r: 255, g: 0, b: 0}},
+        "vert" => {state: "ON", color: { r: 0, g: 255, b: 0}},
+        "rose" => {state: "ON", color: { r: 255, g: 64, b: 64}},
+        "rainbow" => {state: "ON", effect: "Rainbow"}
+      }
+      if states[color].present?
+        send_mqtt(states[color])
+      else
+        send("PRIVMSG #sophiediy :#{color} n'existe pas.")
+      end
+    end
+
+    def send_mqtt(settings)
+      Thread.current[:mqtt_client].publish("lampe_led/5/set", settings.to_json)
     end
   end
 end
